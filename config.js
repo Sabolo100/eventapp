ed2c8810cfc3d27295f5906b513120fa39f94c08
Sabolo100 <szbudahazy@gmail.com>
2025-08-15 18:36:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
+// config.js  (put this in the same folder as index.html)
 export const SUPABASE_URL = 'https://imhdhvvbepgnoqtpnglb.supabase.co';
 export const SUPABASE_ANON_KEY = 'pk_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltaGRodnZiZXBnbm9xdHBuZ2xiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyNDc2NTUsImV4cCI6MjA3MDgyMzY1NX0.t_PkKQql03NTC91ExrOTGzOlRh2h_OGoikF8G1Xk4ow';
 
-
+// optional – only if you’re using the Replit backend to trigger Make
+export const BACKEND_URL = '';
